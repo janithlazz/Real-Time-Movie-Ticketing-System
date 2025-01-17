3f53a79d2250e4c9b53dfdf7cd3df76bd8e40014
Janith-lasindu <janithlasindu15@gmail.com>
2025-01-17 17:44:46 +0530

    Update User class

--- src/User.java
+++ src/User.java
@@ -1,28 +1,10 @@
 public class User {
-    private String userId;
-    private String name;
     private String email;
-    private int phoneNumber;
     private String password;
 
-    public User(String userId, String name, String email, int phoneNumber, String password) {
-        this.userId = userId;
-        this.name = name;
+    public User(String email, String password) {
         this.email = email;
-        this.phoneNumber = phoneNumber;
         this.password = password;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public boolean loginUser(String email, String password){
