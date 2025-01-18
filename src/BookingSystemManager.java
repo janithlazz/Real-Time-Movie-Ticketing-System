c33dfb5705066353a4b6d12d899f87a296f55d6d
Janith-lasindu <janithlasindu15@gmail.com>
2025-01-19 03:34:43 +0530

    Update BookingSystemManager class

--- src/BookingSystemManager.java
+++ src/BookingSystemManager.java
@@ -3,16 +3,13 @@ import java.util.List;
 
 public class BookingSystemManager {
     private List<User> adminUsers;
-    private List<User> customerUsers;
     private List<Movie> movies;
     private List<Theater> theaters;
     private List<Ticket> tickets;
 
-    ArrayList<User> adminUserDataList = new ArrayList<>()
+    ArrayList<User> adminUserDataList = new ArrayList<>();
 
     public void BookingSystem() {
-        this.adminUsers = new ArrayList<>();
-        this.customerUsers = new ArrayList<>();
         this.movies = new ArrayList<>();
         this.theaters = new ArrayList<>();
         this.tickets = new ArrayList<>();
@@ -20,12 +17,11 @@ public class BookingSystemManager {
     public void registerAdmin(User user) {
 
         System.out.println("Admin User registered successfully!");
-        adminUsers.add(user);
+        adminUserDataList.add(user);
     }
     public void registerCustomer(User user) {
 
         System.out.println("Customer User registered successfully!");
-        customerUsers.add(user);
     }
     public User loginUser(String email, String password) {
 
