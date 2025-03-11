c0d4bf1ae721567963c651ef7008fdfd19d10905
Janith-lasindu <janithlasindu15@gmail.com>
2025-03-11 14:23:29 +0530

    Change Admin class sutable for oop

--- src/Admin.java
+++ src/Admin.java
@@ -1,5 +1,7 @@
-public class Admin extends User{
+public class Admin extends User<User> {
 
+
+    public int employeeNumber;
     public Admin(String email, String name, String password) {
         super(email, name, password);
     }
